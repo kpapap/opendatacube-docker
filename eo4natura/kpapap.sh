6baf04f124c14e7d98deaa3a25ff59af1f9b1a49
konstantinos <kostas@norbloc.com>
2024-08-07 09:00:57 +0300

    Add script and makefile

---
+++ eo4natura/kpapap.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Echo lines and exit
+set -ex
+
+docker compose up -d
+docker compose exec -T index datacube system init
+docker compose exec -T index datacube system check
+echo "Adding Product..."
+docker compose exec -T index wget https://eo4naturabucketpublic.s3.us-west-2.amazonaws.com/s2_l2a.odc-product.yml
+docker compose exec -T index datacube product add s2_l2a.odc-product.yml
+echo "Checking STAC API indexing..."
+docker compose exec -T index stac-to-dc \
+    --bbox='23.70,40.49,25.00,41.33' \
+    --catalog-href='https://earth-search.aws.element84.com/v1/' \
+    --limit=1000 \
+    --collections='sentinel-2-l2a' \
+    --datetime='2023-01-01/2023-01-31'
+echo "Initializing Explorer..."
+docker compose exec -T explorer cubedash-gen --init --all
